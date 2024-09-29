54381e19d15ea3fc0c02a6aa4d5f7eb94940f982
jimyan <jimyan@tencent.com>
2024-09-29 09:50:19 +0800

    pm2调整

--- pm2.config.js
+++ pm2.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  apps : [{
-    name  : "litellm",
-    cwd:__dirname + "llmlite",
-    script:"litellm.pm2.start.sh",
-    interpreter:"bash"
-  },
-  {
-    name   : "openWebUI",
-    cwd:__dirname + "/backend",
-    script:"openWebUI.pm2.start.sh",
-    interpreter:"bash"
-  },
-  ]
-}
+	apps: [
+		{
+			name: 'litellm',
+			cwd: __dirname + '/litellm',
+			script: 'litellm.pm2.start.sh',
+			interpreter: 'bash'
+		},
+		{
+			name: 'openWebUI',
+			cwd: __dirname + '/backend',
+			script: 'openWebUI.pm2.start.sh',
+			interpreter: 'bash'
+		}
+	]
+};
